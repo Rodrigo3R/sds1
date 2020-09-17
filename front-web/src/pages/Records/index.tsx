23bde8a5801c35617e8e22e31352c59a5b7e69f6
Rodrigo3R <rodrigo3r7@hotmail.com>
2020-09-17 16:10:25 -0300

    Verificando Versao

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import axios from 'axios';
+import { RecordsResponse } from './types';
 
-const Records = () => (
+const BASE_URL = 'http://localhost:8080';
 
-    <h1>Records</h1>
-);
+const Records = () => {
+
+    const [ recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+    console.log(recordsResponse);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPage=12`)
+        .then(response => setRecordsResponse(response.data));
+    }, []);
+
+    return (
+
+        <div className="page-container">
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TITULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordsResponse?.content.map(record => (
+                        <tr key={record.id}>
+                            <td>20/08/2020 13:45</td>
+                            <td>dodrigo Rocha Rodrigues</td>
+                            <td>44</td>
+                            <td>Xbox</td>
+                            <td>Ação - Aventura</td>
+                            <td>The last of Us 2</td>
+                        </tr>
+                    ))}
+                                              
+                </tbody>
+            </table>
+        </div>
+    )
+};
 
 export default Records;
